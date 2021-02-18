cd63b02c10acfd60ee476fd97876e363c3d47de1
Ch3lLIST4 <herohoang74@gmail.com>
2021-02-18 12:52:52 +0700

    Update counting.py

--- counting.py
+++ counting.py
@@ -3,19 +3,22 @@ from pynput.keyboard import Key, Controller
 import time
 import hashlib 
 
+
+COOLDOWN = 5 # server cooldown
+NUMBER = 1 # starting number
+
+
 keyboard = Controller()
 
 time.sleep(2)
 
-number = 1
-
 while (True):
-	for digit in str(number):
+	for digit in str(NUMBER):
 		keyboard.press(digit)
 		keyboard.release(digit)
 
 	keyboard.press(Key.enter)
 	keyboard.release(Key.enter)
 	
-	number = number + 1
-	time.sleep(1)
+	NUMBER = NUMBER + 1
+	time.sleep(COOLDOWN + 1)
